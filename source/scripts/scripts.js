f90434b0d8a359f4a044d92669990931e4055ee9
KlepnevEA <klepnev-ea@yandex.ru>
2019-09-14 18:35:29 +0300

    18:00

--- source/scripts/scripts.js
+++ source/scripts/scripts.js
@@ -3,10 +3,20 @@ document.addEventListener("DOMContentLoaded", function() {
     $('.select').styler();
 
     let openMenu = (e) => {
+        $('.header').toggleClass('header--open-mobile');
+        $('.header-open').toggleClass('header-open--active')
+    }
+
+    let rangeChange = (e) => {
         e.preventDefault();
-        $('.header').toggleClass('header--open-mobile')
+        let rangeValue = $('#range__input').val()
+        let filLine = $('.experience-js__range-fill');
+        let filLineWidth = $('.experience-js__range').width( );
+        filLine.width( filLineWidth / 3 * rangeValue );
     }
 
     $('.header-open').on('click', openMenu);
-
+    $('.menu__link').on('click', openMenu);
+    
+    $('#range__input').on('input', rangeChange)
 })
